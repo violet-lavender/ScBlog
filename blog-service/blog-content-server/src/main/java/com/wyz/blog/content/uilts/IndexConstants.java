369603e505a674f4b3c19c0065d268f755d08cc8
yqy <repo@yqy.cn>
2025-02-10 20:24:46 +0800

    ES索引常量

---
+++ blog-service/blog-content-server/src/main/java/com/wyz/blog/content/uilts/IndexConstants.java
@@ -0,0 +1,12 @@
+package com.wyz.blog.content.uilts;
+
+/**
+ * 索引常量
+ */
+public class IndexConstants {
+
+	public final static String BLOG_INDEX = "blog";
+
+	public final static String BLOG_SEARCH_KEY = "descriptive_content";
+
+}
