a2bb4f47d8ecf7b53b99cc826222d6993b697652
yqy <repo@yqy.cn>
2025-02-10 20:41:04 +0800

    sdk,软件开发工具包,这里是mq相关

---
+++ blog-service/blog-sdk/src/main/java/com/wyz/blog/sdk/BlogOperateDTO.java
@@ -0,0 +1,30 @@
+package com.wyz.blog.sdk;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 使用 rabbitMQ 传输具体用户对某篇博客进行操作的事件实体类
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlogOperateDTO {
+
+	/**
+	 * 被操作的博客id
+	 */
+	Integer blogId;
+
+	/**
+	 * 执行的用户id，可能为空
+	 */
+	Integer userId;
+
+	/**
+	 * 博客作者id
+	 */
+	Integer authorId;
+
+}
