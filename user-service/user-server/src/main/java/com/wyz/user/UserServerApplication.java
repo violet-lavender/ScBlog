f7fd72f0fa19d9df0344bcdabf9c6a221f277fd2
yqy <repo@yqy.cn>
2025-02-11 17:41:59 +0800

    包管理

--- user-service/user-server/src/main/java/com/wyz/user/UserServerApplication.java
+++ user-service/user-server/src/main/java/com/wyz/user/UserServerApplication.java
@@ -1,9 +1,9 @@
 package com.wyz.user;
 
-import cn.sticki.common.redis.autoconfig.EnableRedisSerialize;
-import cn.sticki.common.tool.mybatisconfig.EnableMybatisPlusIPage;
-import cn.sticki.common.web.advice.EnableDefaultExceptionAdvice;
-import cn.sticki.common.web.advice.EnableDefaultResponseAdvice;
+import com.wyz.common.redis.autoconfig.EnableRedisSerialize;
+import com.wyz.common.tool.mybatisconfig.EnableMybatisPlusIPage;
+import com.wyz.common.web.advice.EnableDefaultExceptionAdvice;
+import com.wyz.common.web.advice.EnableDefaultResponseAdvice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -16,16 +16,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SuppressWarnings("AlibabaCommentsMustBeJavadocFormat")
 @SpringBootApplication
 @EnableAsync // 开启异步控制
-// TODO: 包名修改
-@EnableFeignClients(basePackages = {"cn.sticki.resource.client", "cn.sticki.blog.client"}) // 开启feign
+@EnableFeignClients(basePackages = {"com.wyz.resource.client", "com.wyz.blog.client"}) // 开启feign
 @EnableMybatisPlusIPage // 开启mybatis分页助手
 @EnableRedisSerialize // 开启RedisTemplate序列化配置
 @EnableScheduling // 开启定时任务
 @EnableDefaultExceptionAdvice // 注入默认异常处理器
 @EnableDefaultResponseAdvice // 注入默认包装器
 public class UserServerApplication {
-
     public static void main(String[] args) {
+
         SpringApplication.run(UserServerApplication.class, args);
     }
 
