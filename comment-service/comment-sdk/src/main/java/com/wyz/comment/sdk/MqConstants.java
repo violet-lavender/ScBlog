37b0498af99672fde2f480e74f33916c781907ce
yqy <repo@yqy.cn>
2025-02-11 21:12:23 +0800

    评论模块sdk,mq工具包

---
+++ comment-service/comment-sdk/src/main/java/com/wyz/comment/sdk/MqConstants.java
@@ -0,0 +1,14 @@
+package com.wyz.comment.sdk;
+
+/**
+ * 评论MQ相关常量
+ */
+public class MqConstants {
+
+	public static final String COMMENT_TOPIC_EXCHANGE = "comment.topic";
+
+	public static final String BLOG_COMMENT_DECREASE_KEY = "blog.decrease";
+
+	public static final String BLOG_COMMENT_INCREASE_KEY = "blog.increase";
+
+}
