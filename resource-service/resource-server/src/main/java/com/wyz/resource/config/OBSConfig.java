102c757876eba9bc9a3262dfaa9ad1d9bfbce075
yqy <repo@yqy.cn>
2025-02-13 20:38:57 +0800

    obs配置

--- resource-service/resource-server/src/main/java/com/wyz/resource/config/OBSConfig.java
+++ resource-service/resource-server/src/main/java/com/wyz/resource/config/OBSConfig.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * OBS配置类
+ */
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "obs")
