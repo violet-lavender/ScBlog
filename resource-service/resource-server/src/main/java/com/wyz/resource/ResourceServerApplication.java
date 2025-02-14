1149ce306ed6fb3c36b3b9462f34c42b1a7d3562
yqy <repo@yqy.cn>
2025-02-14 20:59:05 +0800

    资源服务启动类

--- resource-service/resource-server/src/main/java/com/wyz/resource/ResourceServerApplication.java
+++ resource-service/resource-server/src/main/java/com/wyz/resource/ResourceServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author 阿杆
+ * 资源服务启动类
  */
 @SpringBootApplication
 @EnableDefaultExceptionAdvice
